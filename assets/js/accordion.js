18f257b87ebba0ecc26475b58e9e91e87f3b0d50
vwestric <victor.westrich@adwmainz.de>
2023-08-20 23:28:02 +0200

    updated accordion js, background color

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -2,7 +2,7 @@ document.addEventListener("DOMContentLoaded", function() {
     var acc = document.querySelectorAll(".accordion-button");
     for (var i = 0; i < acc.length; i++) {
       acc[i].addEventListener("click", function() {
-        var content = this.nextElementSibling;
+        var content = this.parentElement.nextElementSibling;
         if (content) {
             if (content.style.maxHeight) {
                 content.style.maxHeight = null;
