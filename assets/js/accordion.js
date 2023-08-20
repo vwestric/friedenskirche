a42c591ca793ba7155926099903d8dddd462341f
vwestric <victor.westrich@adwmainz.de>
2023-08-20 22:51:07 +0200

    added accordion faqs

---
+++ assets/js/accordion.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var acc = document.querySelectorAll(".accordion-button");
+    for (var i = 0; i < acc.length; i++) {
+      acc[i].addEventListener("click", function() {
+        var content = this.nextElementSibling;
+        if (content.style.maxHeight) {
+          content.style.maxHeight = null;
+        } else {
+          content.style.maxHeight = content.scrollHeight + "px";
+        }
+      });
+    }
+  });
+  
